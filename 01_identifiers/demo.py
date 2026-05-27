32c2c83bc8706ed9e55208d29067cf8219c5af1d
ravi2krishna <ravi2krishna@gmail.com>
2026-05-27 09:28:25 +0530

    class 3 - Setup Python - VS Code - Syntax - Identifiers

---
+++ 01_identifiers/demo.py
@@ -0,0 +1,50 @@
+# This is Comment - Python Ignores This Line
+# This is Comment - Python Ignores This Line # SyntaxError: invalid syntax
+
+# print function
+print()
+
+print(10)
+print(10.5)
+print(-5)
+
+# print(two) # NameError: name 'two' is not defined
+print('two')
+print("two")
+
+# Identifiers 
+
+today = "wednesday"
+print(today)
+
+# 2day = "wednesday"
+# print(2day) # SyntaxError
+
+day2moro = "thursday"
+print(day2moro)
+
+# $2day = "wednesday" # SyntaxError
+# print($2day)
+
+_2day = "wednesday"
+print(_2day)
+
+# Improper way of naming identifiers
+x = "Ravi"
+y = 24
+z = 9.5
+
+# Proper way of naming identifiers
+student_name = "Ravi"
+student_age = 24
+student_gpa = 9.5
+
+# Static Data 
+pi = 3.14159265359 # Not Recommended
+print(pi)
+
+PI = 3.14159265359 # Recommended
+print(PI)
+
+STUDENT_AADHAR_ID = 1234567890123
+print(STUDENT_AADHAR_ID)
