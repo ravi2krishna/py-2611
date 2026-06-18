9a237a23b2c23b10a99a97419f0eac7881207bdd
ravi2krishna <ravi2krishna@gmail.com>
2026-06-18 09:37:00 +0530

    class 22 - Arbitrary Positional Arguments - Arbitrary Keyword Arguments - return

--- 12_functions/demo.py
+++ 12_functions/demo.py
@@ -146,3 +146,199 @@ def employee_info(emp_name,emp_email,emp_location,emp_mobile,org_name="TCS"): # 
 # Placement Requirement: Default arguments
 def employee_info(emp_name,emp_email,emp_location,org_name="TCS",emp_mobile="88888"): # Non-default argument follows default argument
     print(f"Hi {emp_name} your email is {emp_email}, your mobile number is{emp_mobile} and working for {org_name} at location {emp_location}")
+
+print("=" * 50)
+
+# Without Arbitrary Positional Arguments
+def add_numbers_one(n1):
+    print(n1)
+    
+def add_numbers_two(n1,n2):
+    print(n1+n2)   
+    
+def add_numbers_three(n1,n2,n3):
+    print(n1+n2+n3)     
+    
+add_numbers_one(10)
+add_numbers_two(10,20)
+add_numbers_three(10,20,30)
+# add_numbers_three(10,20,30,40,50) # TypeError: add_numbers_three() takes 3 positional arguments but 5 were given
+
+print("=" * 50)
+
+# With Arbitrary Positional Arguments
+def add_numbers(*numbers):
+    print(numbers)
+    
+add_numbers(10)
+add_numbers(10,20)
+add_numbers(10,20,30)
+add_numbers(10,20,30,40,50)
+
+print("=" * 50)
+
+def add_numbers(*numbers):
+    sum = 0
+    for number in numbers:
+        sum += number
+    print(f"Total Sum is {sum}")
+
+add_numbers(10)
+add_numbers(10,20)
+add_numbers(10,20,30)
+add_numbers(10,20,30,40,50)
+
+print("=" * 50)
+
+def profile(*info):
+    print(info)
+    
+profile("ravi","krishna")
+profile("ravi","krishna","ravi@gmail.com")
+
+print("=" * 50)
+
+# Real World Use Case W.R.T Ecommerce Application (Amazon.in) Cart Functionality 
+def cart_total_value(*products):
+    total_cart_value = 0
+    for product_price in products:
+        total_cart_value += product_price
+    print(f"Total Cart Value Is ₹ {total_cart_value}")
+    
+cart_total_value(34900,11299,2299)
+
+print("=" * 50)
+
+# Arbitrary Keyword Arguments
+def profile(**info):
+    print(info)
+    
+# profile("ravi","krishna") # TypeError: profile() takes 0 positional arguments but 2 were given
+profile(first_name="Ravi")
+profile(first_name="Ravi",last_name="krishna",email="ravi@gmail.com")
+
+print("=" * 50)
+
+def profile(**info):
+    for data in info:
+        print(data)
+
+profile(first_name="Ravi",last_name="krishna",email="ravi@gmail.com")
+
+print("=" * 50)
+
+def profile(**info):
+    for data in info:
+        print(info[data])
+
+profile(first_name="Ravi",last_name="krishna",email="ravi@gmail.com")
+
+print("=" * 50)
+
+# Real World Use Case -> jan=3000, feb=4500, mar=9000
+# Requirement: Calculate Total Transaction Amount and Number Of Transactions Made
+def bank_transactions(**transactions):
+    print(transactions)
+    total_transactions = 0
+    number_of_transactions = 0
+    for transaction in transactions:
+        total_transactions += transactions[transaction]
+        number_of_transactions += 1 
+    print(f"Total transactions amount is {total_transactions} for {number_of_transactions} transactions")
+    
+bank_transactions(jan=3000, feb=4500, mar=9000)    
+bank_transactions(jan=3000, feb=4500, mar=9000, apr=1000, may=2000, june=6000)
+
+print("=" * 50)
+
+# Without return keyword 
+def add(a,b):
+    a + b #  When you don't use a return, by default a function returns None 
+
+add(10,20)
+print(add(10,20))
+
+print("=" * 50)
+
+# Without return keyword and print
+def add(a,b):
+    print(a + b)
+
+add(10,20)
+
+print("=" * 50)
+
+# def special_add(a,b):
+#     print(add(10,20)+b) # TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
+    
+# special_add(100,200) 
+
+print("=" * 50)
+
+# With return keyword
+def add(a,b):
+    return a + b
+
+add(100,200)
+print(add(100,200))
+
+print("=" * 50)
+
+def special_add(a,b):
+    print(add(10,20)+b)
+
+special_add(100,200) 
+
+print("=" * 50)
+
+# Function Composition: function calling another function
+def add(a,b):
+    return a + b
+
+def sub(c,d,e): # add c & d, then minus e --> c + d - e
+    return add(c,d) - e 
+
+print(sub(3,4,5)) # return add(c,d) - e --> return 7 - e --> return 7 - 5 = 2
+
+print("=" * 50)    
+    
+# return - make sure it's the last part of statement to be executed
+def add(a,b):
+    print("Calculation Started")
+    return a + b
+    print("Calculation Completed") # Code is structurally unreachable
+
+print(add(500,500))
+
+print("=" * 50) 
+
+a = 10
+a = 20 
+print(a) 
+
+print("=" * 50) 
+
+# If you have multiple return statements, first return will be considered
+def math_ops(num1,num2):
+    return num1 + num2
+    return num1 * num2
+    return num1 / num2
+
+print(math_ops(10,20))
+
+print("=" * 50) 
+
+# multiple returns are present, and used with conditionals
+def math_ops(num1,num2,operator):
+    if operator == "+":
+        return num1 + num2 
+    elif operator == "-":
+        return num1 - num2 
+    elif operator == "*":
+        return num1 * num2 
+    else:
+        return "Invalid Operator"
+    
+print(math_ops(200,100,"+"))
+print(math_ops(200,100,"*"))
+print(math_ops(200,100,"@"))
