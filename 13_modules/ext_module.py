baeb13eeb4fdd995ba47b56bc1193dd738150942
ravi2krishna <ravi2krishna@gmail.com>
2026-06-23 08:58:38 +0530

    class - 26 - External Modules

---
+++ 13_modules/ext_module.py
@@ -0,0 +1,17 @@
+# External Module 
+
+# import requests # ModuleNotFoundError: No module named 'requests'
+# r = requests.get('https://www.python.org/')
+# # HTTP Status Codes - https://miro.medium.com/v2/0*emwCsVL6_veL1d0I 
+# print("HTTP Status Code",r.status_code)
+
+# Install Module called requests (pip install requests)
+import requests 
+r = requests.get('https://www.python.org/')
+# HTTP Status Codes - https://miro.medium.com/v2/0*emwCsVL6_veL1d0I 
+print("HTTP Status Code",r.status_code)
+
+import requests 
+r = requests.get('https://www.python.org/ravi')
+# HTTP Status Codes - https://miro.medium.com/v2/0*emwCsVL6_veL1d0I 
+print("HTTP Status Code",r.status_code)
