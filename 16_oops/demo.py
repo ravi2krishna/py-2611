2f961970110f8113fde7ceb93e3d3163b71a3812
ravi2krishna <ravi2krishna@gmail.com>
2026-07-01 09:39:17 +0530

    class 33 - Instance Members - Class Members - Static Members - Inheritance

--- 16_oops/demo.py
+++ 16_oops/demo.py
@@ -146,3 +146,157 @@ student_mike = Student("mike","mike@gmail.com")
 student_mike.student_studies() 
 
 print("=" * 50)
+
+# Working With Instance Members 
+class Student:
+    
+    # Constructor i.e __init__() method
+    def __init__(self,student_name,student_email):
+        print("Constructor Called")
+        # Instance Variables self.student_name & self.student_email
+        self.student_name = student_name
+        self.student_email = student_email
+        
+    
+    # Student Does Something - Behaviors / Actions (METHODS)
+    # Instance Method is student_studies()
+    def student_studies(self): # passed "self" nothing but object reference 
+        print("Student Is Studying Python")
+        print("Student Name: ",self.student_name) # Recommended
+        print("Student Email: ",self.student_email) # Not Recommended
+        
+# To Use this class, object is required 
+# student_ravi = Student()
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.student_studies() 
+
+student_john = Student("john","john@gmail.com")
+student_john.student_studies() 
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.student_studies() 
+
+print("=" * 50)
+
+# Working With Class Members 
+class Student:
+    
+    # Class Variable - "shared by all the objects"
+    institute_name = "Digital Institute"
+    
+    # Constructor i.e __init__() method
+    def __init__(self,student_name,student_email):
+        print("Constructor Called")
+        # Instance Variables self.student_name & self.student_email
+        self.student_name = student_name
+        self.student_email = student_email
+        
+    
+    # Student Does Something - Behaviors / Actions (METHODS)
+    # Instance Method is student_studies()
+    def student_studies(self): # passed "self" nothing but object reference 
+        print("Student Is Studying Python")
+        print("At Institute: ",self.institute_name) # Not Recommended
+        print("Student Name: ",self.student_name) # Recommended
+        print("Student Email: ",self.student_email) # Recommended
+        print("At Institute: ",Student.institute_name) # Recommended
+        
+    # Class Method 
+    @classmethod
+    def change_institute_name(cls,new_institute_name):
+        cls.institute_name = new_institute_name
+        # Accessing instance data inside a class method gives error 
+        # print("Student Name: ",self.student_name) # NameError: name 'self' is not defined
+        
+        
+# To Use this class, object is required 
+# student_ravi = Student()
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.student_studies() 
+
+student_john = Student("john","john@gmail.com")
+student_john.student_studies() 
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.student_studies() 
+
+# Class Method 
+Student.change_institute_name("New Institute")
+
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.student_studies() 
+
+student_john = Student("john","john@gmail.com")
+student_john.student_studies() 
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.student_studies() 
+
+
+print("=" * 50)
+
+# Working With Static Members 
+class Student:
+    
+    # Class Variable - "shared by all the objects"
+    institute_name = "Digital Institute"
+    
+    # Constructor i.e __init__() method
+    def __init__(self,student_name,student_email):
+        print("Constructor Called")
+        # Instance Variables self.student_name & self.student_email
+        self.student_name = student_name
+        self.student_email = student_email
+        
+    
+    # Student Does Something - Behaviors / Actions (METHODS)
+    # Instance Method is student_studies()
+    def student_studies(self): # passed "self" nothing but object reference 
+        print("Student Is Studying Python")
+        print("At Institute: ",self.institute_name) # Not Recommended
+        print("Student Name: ",self.student_name) # Recommended
+        print("Student Email: ",self.student_email) # Recommended
+        print("At Institute: ",Student.institute_name) # Recommended
+        
+    # Class Method 
+    @classmethod
+    def change_institute_name(cls,new_institute_name):
+        cls.institute_name = new_institute_name
+        # Accessing instance data inside a class method gives error 
+        # print("Student Name: ",self.student_name) # NameError: name 'self' is not defined
+    
+    # Static Method 
+    @staticmethod
+    def something_not_related_to_class_object(a,b):
+        print("I Do Something that is not associated with Classes & Objects")
+        print("Sum Of Numbers: ",(a+b))
+        
+        
+# To Use this class, object is required 
+# student_ravi = Student()
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.student_studies() 
+
+student_john = Student("john","john@gmail.com")
+student_john.student_studies() 
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.student_studies() 
+
+# Class Method 
+Student.change_institute_name("New Institute")
+
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.student_studies() 
+
+student_john = Student("john","john@gmail.com")
+student_john.student_studies() 
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.student_studies() 
+
+
+# Static Method
+Student.something_not_related_to_class_object(10,10)
+
+print("=" * 50)
---
+++ 17_inheritance/without.py
@@ -0,0 +1,148 @@
+# Without Inheritance - We Write Same Functionalities again and again multiple times 
+
+# LMS Application (Application For Watching Course Videos)
+
+# Student       ->  Watch Videos 
+
+# VideoAdmin    ->  Watch Videos & Add Videos  
+
+# SuperAdmin    ->  Watch Videos, Add Videos & Delete Videos 
+
+class Student:
+    # Watch Videos 
+    def watch_videos(self):
+        print("=" * 50)
+        print("Functionality For Watching Videos")
+        print("=" * 50)
+        print("W")
+        print("A")
+        print("T")
+        print("C")
+        print("H")
+        print("I")
+        print("G")
+        print("V")
+        print("I")
+        print("D")
+        print("E")
+        print("O")
+        print("..............") # real world code is say 2000 lines of code 
+        
+
+class VideoAdmin:
+    # Watch Videos 
+    def watch_videos(self):
+        print("=" * 50)
+        print("Functionality For Watching Videos")
+        print("=" * 50)
+        print("W")
+        print("A")
+        print("T")
+        print("C")
+        print("H")
+        print("I")
+        print("G")
+        print("V")
+        print("I")
+        print("D")
+        print("E")
+        print("O")
+        print("..............") # Repeat Same 2000 lines of code again 
+        
+        
+    # Add Videos 
+    def add_videos(self):
+        print("=" * 50)
+        print("Functionality For Adding Videos")
+        print("=" * 50)
+        print("A")
+        print("D")
+        print("D")
+        print("I")
+        print("N")
+        print("G")
+        print("V")
+        print("I")
+        print("D")
+        print("E")
+        print("O")
+        print("..............") # real world code is say 2000 lines of code
+
+class SuperAdmin:
+    # Watch Videos 
+    def watch_videos(self):
+        print("=" * 50)
+        print("Functionality For Watching Videos")
+        print("=" * 50)
+        print("W")
+        print("A")
+        print("T")
+        print("C")
+        print("H")
+        print("I")
+        print("G")
+        print("V")
+        print("I")
+        print("D")
+        print("E")
+        print("O")
+        print("..............") # Repeat Same 2000 lines of code again 
+        
+        
+    # Add Videos 
+    def add_videos(self):
+        print("=" * 50)
+        print("Functionality For Adding Videos")
+        print("=" * 50)
+        print("A")
+        print("D")
+        print("D")
+        print("I")
+        print("N")
+        print("G")
+        print("V")
+        print("I")
+        print("D")
+        print("E")
+        print("O")
+        print("..............") # Repeat Same 2000 lines of code again 
+        
+    # Delete Videos 
+    def delete_videos(self):
+        print("=" * 50)
+        print("Functionality For Deleting Videos")
+        print("=" * 50)
+        print("D")
+        print("E")
+        print("L")
+        print("E")
+        print("T")
+        print("I")
+        print("N")
+        print("G")
+        print("V")
+        print("I")
+        print("D")
+        print("E")
+        print("O")
+        print("..............") # real world code is say 2000 lines of code
+        
+    # Now Technically we have 6000 Lines Of Duplicate Code 
+    
+
+print("Student User")
+student_user = Student()
+student_user.watch_videos()
+
+print("Video Admin User")
+video_admin_user = VideoAdmin()
+video_admin_user.watch_videos()
+video_admin_user.add_videos()
+
+print("Super Admin User")
+super_admin_user = SuperAdmin()
+super_admin_user.watch_videos()
+super_admin_user.add_videos()
+super_admin_user.delete_videos()
+
+
